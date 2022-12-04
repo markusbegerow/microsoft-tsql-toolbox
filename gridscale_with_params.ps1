a14efb1915f08f4e47bb5f7ccaa02d26221ea56f
Markus Begerow <44146279+markusbegerow@users.noreply.github.com>
2022-12-04 15:16:04 +0100

    Handling Gridscale VM via T-SQL & PowerShell

---
+++ gridscale_with_params.ps1
@@ -0,0 +1,18 @@
+param (
+$MyUrl,
+$XAuthUserId,
+$XAuthToken,
+$Param
+)
+
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("X-Auth-UserId", $XAuthUserId)
+$headers.Add("X-Auth-Token", $XAuthToken)
+$headers.Add("Content-Type", "application/json")
+
+$body = "{
+`n    `"power`": $Param
+`n}"
+
+$response = Invoke-RestMethod $MyUrl -Method 'PATCH' -Headers $headers -Body $body
+$response | ConvertTo-Json
